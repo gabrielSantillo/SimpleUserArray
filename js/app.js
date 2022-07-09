03795a1e98f80bee8fcfd275615a54641daad65f
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-09 17:14:24 -0600

    Adding comments

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+//This is ten names and ages respectively
 let user_names = [`Gabriel`, `Alex`, `Satinder`, `Tyler`, `Nadine`, `Zach`, `Colleen`, `Josh`, `James`, `Hani`];
 let user_ages = [26, 35, 30, 30, 40, 29, 30, 25, 30, 17];
 
+//This while loop is searching for ages that are at least 18 and then printing out the names of who are above 18
 let counter = 0;
 while(counter < user_ages.length) {
     if(user_ages[counter] >= 18){
@@ -9,6 +11,8 @@ while(counter < user_ages.length) {
    
     counter++;
 }
+
+//This for loop is searching for ages that are under 18 and then printing out the names of who are under 18
 
 for(let counter = 0; counter < user_ages.length; counter++) {
     if(user_ages[counter] < 18){
